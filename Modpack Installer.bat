7e50cf7c03dc9637f7b11fcac010b9701d261970
Vivian Aktjef <vivian-aktjef@t-online.de>
2023-06-20 18:07:54 +0200

    Modpack Installer.bat aktualisiert
    Batch-Skript erweitert.

--- Modpack Installer.bat
+++ Modpack Installer.bat
@@ -1,5 +1,5 @@
 @echo off
-MODE 234, 50
+MODE 320, 50
 TITLE Revelation++ Installer
 
 COLOR 09
@@ -36,28 +36,56 @@ ECHO.
 
 COLOR 06
 
+ECHO [!date! - !time!] Beginnen mit Installation...
+ECHO [!date! - !time!] Verzeichnisse werden erstellt...
+
 md "RevelationPlusPlus"
 md "RevelationPlusPlus\libraries"
-md "RevelationPlusPlus\minecraft\config\"
+md "RevelationPlusPlus\minecraft\config"
+md "RevelationPlusPlus\minecraft\config\alarms\assets\energycontrol\sounds"
 md "RevelationPlusPlus\minecraft\config\immersiverailroading"
+md "RevelationPlusPlus\minecraft\config\buildcraft"
+md "RevelationPlusPlus\minecraft\config\brandon3055"
+md "RevelationPlusPlus\minecraft\config\aroma1997"
 md "RevelationPlusPlus\minecraft\mods"
 md "RevelationPlusPlus\minecraft\marytts"
 md "RevelationPlusPlus\minecraft\resourcepacks"
 md "RevelationPlusPlus\minecraft\scripts"
-md "RevelationPlusPlus\natives"
 md "RevelationPlusPlus\patches"
+md "RevelationPlusPlus\jarmods"
+
+ECHO [!date! - !time!] Dateien werden angefordert und heruntergeladen...
+
+SET /a File = 1
 
 FOR /F "tokens=1,2" %%x IN (modpack_files.txt) DO (
-	echo [!date! - !time!] %%y wird von %%x heruntergeladen.
-	powershell -Command "Invoke-WebRequest --% %%x -Outfile %%y -TimeoutSec 0"
+	ECHO [!date! - !time!] [!File! von %Lines%] %%y wird von %%x heruntergeladen.
+	powershell -Command "Invoke-WebRequest %%x -Outfile %%y -TimeoutSec 0"
+	SET /A File += 1
 )
 
-ECHO [%DATE%-%TIME%] Aufräumen...
+ECHO [!date! - !time!] Aufräumen...
+
 DEL modpack_files.txt
 DEL version.txt
 
 COLOR 0A
 
-ECHO [%DATE%-%TIME%] Installation abgeschlossen!
+ECHO [!date! - !time!] Installation abgeschlossen!
 
-PAUSE
+CALL :MsgBox "Soll Revelation++ nun gestartet werden?"  "VBYesNo+VBQuestion" "Installation abgeschlossen!"
+IF errorlevel 7 (
+	EXIT /B
+)
+ELSE IF errorlevel 6 (
+	CD..
+	START MultiMC.exe -l "RevelationPlusPlus"
+	EXIT /B
+)
+
+:MsgBox prompt type title
+    SETLOCAL enableextensions
+    SET "tempFile=Installation.vbs"
+    >"%tempFile%" ECHO(WScript.Quit msgBox("%~1",%~2,"%~3") & cscript //nologo //e:vbscript "%tempFile%"
+    SET "exitCode=%errorlevel%" & DEL "%tempFile%" >NUL 2>NUL
+    ENDLOCAL & EXIT /B %exitCode%
