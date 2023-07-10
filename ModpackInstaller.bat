17c0bf9adcc69ea72dab5b2238f4ce83c400718e
Vivian Aktjef <vivian-aktjef@t-online.de>
2023-07-10 13:25:41 +0200

    ModpackInstaller.bat aktualisiert
    Code überarbeitet.

--- ModpackInstaller.bat
+++ ModpackInstaller.bat
@@ -23,8 +23,8 @@ ECHO ^+-------------------------------------------------------------------------
 ECHO.
 
 SETLOCAL enabledelayedexpansion
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ViVBling/RevelationPlusPlus/main/version.txt -Outfile version.txt"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ViVBling/RevelationPlusPlus/main/modpack_files.txt -TimeoutSec 0 -Outfile modpack_files.txt"
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/ViVBling/RevelationPlusPlus/main/version.txt -Outfile version.txt"
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/ViVBling/RevelationPlusPlus/main/modpack_files.txt -TimeoutSec 0 -Outfile modpack_files.txt"
 
 ECHO|SET /p=Folgende Modpack-Version wurde gefunden und wird installiert: 
 type version.txt
@@ -40,28 +40,31 @@ ECHO [!date! - !time!] Beginnen mit Installation...
 ECHO [!date! - !time!] Verzeichnisse werden erstellt...
 
 md "RevelationPlusPlus"
-md "RevelationPlusPlus\libraries"
-md "RevelationPlusPlus\minecraft\config"
-md "RevelationPlusPlus\minecraft\config\alarms\assets\energycontrol\sounds"
-md "RevelationPlusPlus\minecraft\config\immersiverailroading"
-md "RevelationPlusPlus\minecraft\config\buildcraft"
-md "RevelationPlusPlus\minecraft\config\brandon3055"
-md "RevelationPlusPlus\minecraft\config\aroma1997"
-md "RevelationPlusPlus\minecraft\config\projectE"
-md "RevelationPlusPlus\minecraft\config\enderio"
-md "RevelationPlusPlus\minecraft\config\cofh\thermalexpansion"
-md "RevelationPlusPlus\minecraft\config\cofh\thermalfoundation"
-md "RevelationPlusPlus\minecraft\config\cofh\world"
-md "RevelationPlusPlus\minecraft\config\CustomMainMenu"
-md "RevelationPlusPlus\minecraft\config\gulliverreborn"
-md "RevelationPlusPlus\minecraft\config\rftools"
-md "RevelationPlusPlus\minecraft\mods"
-md "RevelationPlusPlus\minecraft\marytts"
-md "RevelationPlusPlus\minecraft\resourcepacks"
-md "RevelationPlusPlus\minecraft\scripts"
-md "RevelationPlusPlus\minecraft\journeymap\config\5.7"
-md "RevelationPlusPlus\patches"
-md "RevelationPlusPlus\jarmods"
+
+CD RevelationPlusPlus
+
+md "libraries"
+md "minecraft\config"
+md "minecraft\config\alarms\assets\energycontrol\sounds"
+md "minecraft\config\immersiverailroading"
+md "minecraft\config\buildcraft"
+md "minecraft\config\brandon3055"
+md "minecraft\config\aroma1997"
+md "minecraft\config\projectE"
+md "minecraft\config\enderio"
+md "minecraft\config\cofh\thermalexpansion"
+md "minecraft\config\cofh\thermalfoundation"
+md "minecraft\config\cofh\world"
+md "minecraft\config\CustomMainMenu"
+md "minecraft\config\gulliverreborn"
+md "minecraft\config\rftools"
+md "minecraft\mods"
+md "minecraft\marytts"
+md "minecraft\resourcepacks"
+md "minecraft\scripts"
+md "minecraft\journeymap\config\5.7"
+md "patches"
+md "jarmods"
 
 ECHO [!date! - !time!] Dateien werden angefordert und heruntergeladen...
 
@@ -69,9 +72,13 @@ SET /a File = 1
 
 FOR /F "tokens=1,2" %%x IN (modpack_files.txt) DO (
 	ECHO [!date! - !time!] [!File! von %Lines%] %%y wird von %%x heruntergeladen.
-	powershell -Command "Invoke-WebRequest %%x -Outfile %%y -TimeoutSec 0"
+	powershell -Command "Invoke-WebRequest -Uri %%x -Outfile %%y -TimeoutSec 0"
 	SET /A File += 1
 )
+
+CD..\..\icons
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/ViVBling/RevelationPlusPlus/main/4387a02d-7f5a-465a-bf2b-da4df1ae86d0.png -Outfile icon.png"
+CD..\instances
 
 ECHO [!date! - !time!] Aufräumen...
 
@@ -84,14 +91,14 @@ ECHO [!date! - !time!] Installation abgeschlossen!
 
 CALL :MsgBox "Soll Revelation++ nun gestartet werden?"  "VBYesNo+VBQuestion" "Installation abgeschlossen!"
 IF errorlevel 7 (
-	DEL Installer.bat
+	DEL ModpackInstaller.bat
 	EXIT /B
 )
 IF errorlevel 6 (
 	CD..
 	START "" MultiMC.exe -l "RevelationPlusPlus"
 	CD instances
-	DEL Installer.bat
+	DEL ModpackInstaller.bat
 	EXIT /B
 )
 
